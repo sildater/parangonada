18fd1a1526a4506150490ac83cb4bfeabb3710dc
sildater <41552783+sildater@users.noreply.github.com>
2020-11-11 16:30:54 +0100

    indextest

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ from flask import Flask, escape, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
     return render_template("index.html")
+
+@app.route('/test')
+def indextest():
+    return render_template("indextest.html")
 
 if __name__ == "__main__":
     app.run()
